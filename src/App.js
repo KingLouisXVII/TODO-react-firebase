dfc36f310a40a6d66aa32061cba8676b15e5bb8e
Daniel Jakobian <daniel.jakobian@datagraphis.de>
2019-10-13 00:04:24 +0200

    [ADD] drag and drop and delete button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
 import './App.scss';
 
 
@@ -19,9 +21,20 @@ function App() {
 
 
     useEffect(() => {
+        const localTodos = localStorage.getItem('todos');
+        localTodos !== null ?
+            setTodos(JSON.parse(localTodos))
+            :
+            setTodos([]);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+
+    useEffect(() => {
         const allLists = [...lists];
         const activeList = allLists.filter(list => list.active === true);
-        console.log(activeList)
         const activeTodos = activeList[0].todos; 
         setTodos(activeTodos);
     }, [lists])
@@ -32,6 +45,10 @@ function App() {
         const newTodo = {name:todo, completed:false};
         newTodos.push(newTodo);
         setTodos(newTodos);
+    }
+
+    function deleteTodo(i) {
+        setTodos(todos => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
     }
 
     function onChange(e) {
@@ -45,12 +62,42 @@ function App() {
         }
     }
 
+    function toggleTodo(i) {
+        const newTodos = [...todos];
+        newTodos[i].completed = !newTodos[i].completed;
+        newTodos.sort(function(a,b){return a.completed-b.completed});
+        setTodos(newTodos);
+    }
+
+    function onDragEnd(result) {
+        if (!result.destination) {
+            return;
+        }
+        if (result.destination.index === result.source.index) {
+            return;
+        }
+        const newTodos  = reorder(
+            todos,
+            result.source.index,
+            result.destination.index
+        );
+        setTodos(newTodos);
+    }
+
+    function reorder(list, startIndex, endIndex) {
+        const result = Array.from(list);
+        const [removed] = result.splice(startIndex, 1);
+        result.splice(endIndex, 0, removed);
+        return result;
+    };
+
+
     return (
         <div className="App">
             <div id="sidebar">
                 {
                     lists.map((list,i) =>
-                        <div>{list.name}</div>
+                        <div key={i}>{list.name}</div>
                     )}
                     <input
                         type="text"
@@ -60,13 +107,36 @@ function App() {
                     />
                 </div>
                 <div id="todos">
-                    <ul>
-                        {
-                            todos.map((todo,i) =>
-                                <li>{todo.name}</li>
-                            )
-                        }
-                    </ul>
+                    <DragDropContext onDragEnd={onDragEnd}>
+                        <Droppable droppableId="list">
+                            {provided => (
+                                <ul ref={provided.innerRef} {...provided.droppableProps}>
+                                    {
+                                        todos.map((todo,i) =>
+                                            <Draggable key={i.toString()} draggableId={i.toString()} index={i}>
+                                                {provided => (
+                                                    <div className="todo"
+                                                        key={i}
+                                                        ref={provided.innerRef}
+                                                        {...provided.draggableProps}
+                                                        {...provided.dragHandleProps}
+                                                    >
+                                                        <li
+                                                            onClick={e=>toggleTodo(i)}
+                                                            className={todo.completed?'completed':''}
+                                                        >{todo.name}</li>
+                                                        <div onClick={e=>deleteTodo(i)} className="delete">X</div>
+                                                    </div>
+                                                )
+                                                }
+                                            </Draggable>
+                                        )
+                                    }
+                                    {provided.placeholder}
+                                </ul>
+                            )}
+                        </Droppable>
+                    </DragDropContext>
                 </div>
             </div>
     );
