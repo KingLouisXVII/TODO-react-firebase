daf289c85b7523362804bdb04846bdbe81b24227
Daniel Jakobian <daniel.jakobian@datagraphis.de>
2019-10-18 13:20:52 +0200

    oder function layout in app.js

--- src/App.js
+++ src/App.js
@@ -43,6 +43,21 @@ function App() {
     localStorage.setItem('lists', JSON.stringify(lists));
   }, [lists]);
 
+  function onChange(e) {
+    setTodo(e.target.value);
+  }
+
+  function handleKeyDown(e) {
+    if (e.key === 'Enter' && e.target.name === 'todo' && todo.length !== 0) {
+      addTodo(e);
+      setTodo('');
+    }
+    if (e.key === 'Enter' && e.target.name === 'list' && todo.length !== 0) {
+      addList();
+      setTodo('');
+    }
+  }
+
   function addEmptyList() {
     setAddingList(true);
   }
@@ -63,6 +78,16 @@ function App() {
     setActive(list);
   }
 
+  function deleteList(list) {
+    const allLists = {...lists};
+    delete allLists[list];
+    setLists(allLists);
+  }
+
+  function addEmptyTodo() {
+    setAddingTodo(true);
+  }
+
   function addTodo() {
     const allLists = {...lists};
     const todos = allLists[active].todos;
@@ -75,34 +100,6 @@ function App() {
     setAddingTodo(false);
   }
 
-  function deleteTodo(i) {
-    const allLists = {...lists};
-    const todos = allLists[active].todos;
-    todos.splice(i,1);
-    setLists(allLists);
-  }
-
-  function deleteList(list) {
-    const allLists = {...lists};
-    delete allLists[list];
-    setLists(allLists);
-  }
-
-  function onChange(e) {
-    setTodo(e.target.value);
-  }
-
-  function handleKeyDown(e) {
-    if (e.key === 'Enter' && e.target.name === 'todo' && todo.length !== 0) {
-      addTodo(e);
-      setTodo('');
-    }
-    if (e.key === 'Enter' && e.target.name === 'list' && todo.length !== 0) {
-      addList();
-      setTodo('');
-    }
-  }
-
   function toggleTodo(i) {
     const allLists = {...lists};
     const todos = allLists[active].todos;
@@ -111,8 +108,24 @@ function App() {
     setLists(allLists);
   }
 
-  function addEmptyTodo() {
-    setAddingTodo(true);
+  function deleteTodo(i) {
+    const allLists = {...lists};
+    const todos = allLists[active].todos;
+    todos.splice(i,1);
+    setLists(allLists);
+  }
+
+  function removeCompleted(todo) {
+    return todo.completed === false;
+  } 
+
+  function clearDone() {
+    const allLists = {...lists};
+    const newTodos = allLists[active].todos.filter(removeCompleted);
+    allLists[active].todos = newTodos;
+    setLists(allLists);
+    setTodo('');
+    setAddingTodo(false);
   }
 
   function onDragEnd(result) {
@@ -145,19 +158,6 @@ function App() {
       result.destination.index
     );
     setLists(allLists);
-  }
-
-  function removeCompleted(todo) {
-    return todo.completed === false;
-  } 
-
-  function clearDone() {
-    const allLists = {...lists};
-    const newTodos = allLists[active].todos.filter(removeCompleted);
-    allLists[active].todos = newTodos;
-    setLists(allLists);
-    setTodo('');
-    setAddingTodo(false);
   }
 
   return (
