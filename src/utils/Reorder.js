1ec4e3c807bdc4b610fe706b27f95f35d2ab0a47
Daniel Jakobian <daniel.jakobian@datagraphis.de>
2019-11-11 09:28:16 +0100

    Remove todos ul scrollbar

--- src/utils/Reorder.js
+++ src/utils/Reorder.js
@@ -7,6 +7,7 @@ export const reorderTodos = (list, startIndex, endIndex) => {
 };
 
 export const reorderLists = (list, startIndex, endIndex) => {
+  console.log(list, startIndex, endIndex)
   const result = Object.entries(list);
   const [removed] = result.splice(startIndex, 1);
   result.splice(endIndex, 0, removed);
